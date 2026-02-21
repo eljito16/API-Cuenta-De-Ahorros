28ad98920e22e4b99bfb058b328f73807b30b0fc
Juan Jose Arboleda <juanjosearboledaruiz16@gmail.com>
2026-02-20 23:57:52 -0500

    feat. se agrego el gitinore y se borraron archivos innecesarios

--- obj/Debug/net10.0/CuentaApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/CuentaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CuentaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918ddb57b8118dce826470c538365af08fad9ec3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CuentaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CuentaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
